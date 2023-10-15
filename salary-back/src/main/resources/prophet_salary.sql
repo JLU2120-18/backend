2637af897e1bee6016a0adcdadfc9a2dc95bc4c0
Jialin2002 <131441466+Jialin2002@users.noreply.github.com>
2023-10-15 14:55:39 +0800

    添加SQL文件
    添加了prophet_salary.sql

---
+++ salary-back/src/main/resources/prophet_salary.sql
@@ -0,0 +1,47 @@
+create database prophet_salary;
+
+use prophet_salary;
+
+create table auth(
+    id varchar(32) primary key,
+    username varchar(32) comment '用户名',
+    password varchar(64) comment '密码',
+    role varchar(16) comment '角色: employee-普通员工    commission-委托员工    payroll-管理员'
+);
+
+create table user(
+    id varchar(32) primary key,
+    username varchar(32) comment '用户名',
+    address varchar(256) comment '通讯地址',
+    socsec_id varchar(32) comment '社保',
+    tax_rate int comment '税收',
+    other_cast int comment '其他扣除',
+    phone varchar(11) comment '电话',
+    hour_wage int comment '时薪',
+    salary int comment '薪资',
+    commission_rate int comment '佣金率',
+    duration_limit int comment '工作时间限制',
+    type varchar(16) comment '员工类型: salary-工资制   commission-委托制    wage-时薪制',
+    payment varchar(16) comment '支付类型: mail-邮寄    receive-领取    bank-银行',
+    mail_address varchar(32) comment '邮箱地址',
+    bank_name varchar(16) comment '银行',
+    bank_account varchar(16) comment '银行账号'
+);
+
+create table purchase_order(
+    id varchar(32) primary key,
+    employee_id varchar(32) comment '员工id',
+    product_name varchar(32) comment '产品名称',
+    phone varchar(11) comment '电话',
+    address varchar(256) comment '地址',
+    date varchar(32) comment '日期'
+);
+
+create table time_card(
+    id varchar(32) primary key,
+    is_save boolean comment '是否保存',
+    start_time varchar(32) comment '开始时间',
+    end_time varchar(32) comment '结束时间',
+    duration int comment '工作时长'
+);
+
