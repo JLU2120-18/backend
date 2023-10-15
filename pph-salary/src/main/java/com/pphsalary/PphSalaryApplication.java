1ddbd5a022448b18c5428374bc7be0ffef897d61
Llj <105537986+LongJie-Lu@users.noreply.github.com>
2023-10-15 12:51:03 +0800

    Add files via upload

---
+++ pph-salary/src/main/java/com/pphsalary/PphSalaryApplication.java
@@ -0,0 +1,13 @@
+package com.pphsalary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PphSalaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PphSalaryApplication.class, args);
+    }
+
+}
---
+++ pph-salary/src/test/java/com/pphsalary/PphSalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pphsalary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PphSalaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
