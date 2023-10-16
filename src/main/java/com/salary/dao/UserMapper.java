2409c91b99fb6c5ae97b47ad4a58f6bb5c1b6f5a
xianzhi <engineer@xianzhi.cn>
2023-10-16 16:38:18 +0800

    1.修改了数据库表一些字段类型 2.添加了@Mapper注解

--- src/main/java/com/salary/dao/UserMapper.java
+++ src/main/java/com/salary/dao/UserMapper.java
@@ -1,13 +1,15 @@
 package com.salary.dao;
 
 import com.salary.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author Jialin
  * @create 2023-10-16 13:24
  */
+
+@Mapper
 public interface UserMapper {
     int insert(User user);
     User selectUserById(String id);
-
 }
