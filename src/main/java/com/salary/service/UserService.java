f44978cfb8435f90ce1bfafa68623c39f4a8d0b9
xianzhi <engineer@xianzhi.cn>
2023-10-16 14:19:18 +0800

    添加了维护员工信息中的创建员工功能

---
+++ src/main/java/com/salary/service/UserService.java
@@ -0,0 +1,12 @@
+package com.salary.service;
+
+import com.salary.pojo.JWT;
+import com.salary.pojo.User;
+
+/**
+ * @author Jialin
+ * @create 2023-10-16 13:07
+ */
+public interface UserService {
+    JWT createEmployee(User user);
+}
