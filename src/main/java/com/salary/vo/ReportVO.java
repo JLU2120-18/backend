13216ca57be47f42b8d44181f19cec4a037e6395
xianzhi <engineer@xianzhi.cn>
2023-10-19 20:30:30 +0800

    Final Version

--- src/main/java/com/salary/vo/ReportVO.java
+++ src/main/java/com/salary/vo/ReportVO.java
@@ -4,9 +4,6 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-/**
- * 描述：
- */
 @Data
 public class ReportVO<T> implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -20,7 +17,4 @@ public class ReportVO<T> implements Serializable {
         return new ReportVO<>(data);
     }
 
-//    public static <T> AdminReportVO<T> error(){
-//        return new AdminReportVO<>(ResponeEnum.ERROR.getDesc());
-//    }
 }
